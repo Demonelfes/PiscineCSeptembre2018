11570ecdb4f765159e2ecf80bd733b348168d092
Alopez <alexis.lopez.0605@gmail.com>
2019-04-12 19:20:38 +0200

    modif memccpy

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -20,8 +20,9 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	while (i < n)
 	{
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
-		i++;
 		if (((unsigned char *)src)[i] == c)
 			return ((void *)(dst + i + 1));
+		i++;
 	}
+	return (NULL);
 }
