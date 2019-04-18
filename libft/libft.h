1a1db15a20d3fb2ff7e586fd6b0f6ff3a7ad8a38
Alopez <alexis.lopez.0605@gmail.com>
2019-04-18 11:07:24 +0200

    add functions list day11piscine

--- libft/libft.h
+++ libft/libft.h
@@ -43,6 +43,10 @@ void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+void				ft_lst_pushback(t_list **begin_list, void *data);
+void				ft_lst_pushfront(t_list **begin_list, void *data);
+void				ft_lst_reverse(t_list **begin_list);
+void				ft_lst_sort(t_list **begin_list, int (*cmp)());
 void				*ft_memset(void *b, int c, size_t len);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
@@ -84,5 +88,6 @@ size_t				ft_strlcat(char *dest, const char *src, size_t size);
 size_t				ft_strlen(const char *str);
 t_list				*ft_lstnew(void const *content, size_t content_size);
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
+t_list				*ft_create_elem(void *data);
 
 #endif
