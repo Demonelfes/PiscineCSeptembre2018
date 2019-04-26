f2b71d1d22292b2567c65e1fc9fa330b5492842a
Alexis LOPEZ <allopez@e3r5p8.42.fr>
2019-04-26 16:15:35 +0200

    push gnl en 2 fonction, il ne reste que des leaks

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -6,31 +6,26 @@
 /*   By: allopez <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 14:35:27 by allopez           #+#    #+#             */
-/*   Updated: 2019/04/26 13:42:09 by allopez          ###   ########.fr       */
+/*   Updated: 2019/04/26 16:15:10 by allopez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int			check_error(int fd, char **str, char **line)
-{
-	if (!*str)
-	{
-		if (!(*str = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
-			return (-1);
-	}
-	return (fd == -1 || line == NULL ? -1 : 0);
-}
-
 char		*readline(char *str, int fd)
 {
 	char	buff[BUFF_SIZE + 1];
+	char	*tmp;
 	int		ret;
 
-	while ((ret = read(fd, buff, BUFF_SIZE) > 0))
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[ret] = '\0';
-		str = ft_strjoin(str, buff);
+		tmp = ft_strjoin(str, buff);
+		free(str);
+		str = tmp;
+		if (str[0] == '\0')
+			ft_strdel(&str);
 	}
 	return (str);
 }
@@ -40,13 +35,13 @@ int			get_next_line(const int fd, char **line)
 	static char		*str;
 	int				i;
 
-	if (check_error(fd, &str, line) == -1)
-		return (-1);
+	if (!str)
+		if (!(str = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+			return (0);
 	if (*str)
 		ft_strcpy(*line, str);
-	i = 0;
 	str = readline(str, fd);
-	if (str[i])
+	if (str[i] && !(i = 0))
 	{
 		while (str[i] != '\n' && str[i])
 			i++;
@@ -61,5 +56,5 @@ int			get_next_line(const int fd, char **line)
 	}
 	else
 		(*line) = ft_strnew(1);
-	return (0);
+	return (fd - 1 || line == NULL ? -1 : 0);
 }
--- GetNextLine/main.c
+++ GetNextLine/main.c
@@ -1,17 +1,17 @@
 #include "get_next_line.h"
 
-int     main()
+int     main(int ac, char **av)
 {
 	int     fd;
+	int ret;
 	char    *line;
 
 	fd = open("test.txt", O_RDONLY);
 
-	int ret;
-	while ((ret = get_next_line(fd, &line)) > 0) 
-	{
-		printf("%s\n", line);
-		printf("%d\n", ret);
-		free(line);
-	}
+	while (1)
+		while ((ret = get_next_line(fd, &line)) > 0)
+		{
+			ft_putstr(line);
+		}
+	return (0);
 }
