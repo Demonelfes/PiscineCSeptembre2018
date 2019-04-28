07707b55de3162261f3e5ca23246f68d2599f8c2
Alopez <alexis.lopez.0605@gmail.com>
2019-04-28 18:16:31 +0200

    Bugs trouves, voir les comm dans le .c

--- GetNextLine/get_next_line.c
+++ GetNextLine/get_next_line.c
@@ -34,6 +34,7 @@ int			get_next_line(const int fd, char **line)
 {
 	static char		*str;
 	int				i;
+	char	*tmp;
 
 	if (!str)
 		if (!(str = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
@@ -45,16 +46,26 @@ int			get_next_line(const int fd, char **line)
 	{
 		while (str[i] != '\n' && str[i])
 			i++;
-//		if (i == 0)
-//			ft_bzero((*line), 1);
-//		else
-//		{
+		if (i == 0)
+			ft_bzero((*line), 1);
+		else
+		{
 			(*line) = ft_strsub(str, 0, i);
+			tmp = ft_strdup((*line));
+			free((*line));
+			(*line) = tmp;
 			str = &str[i + 1];
-//		}
+			if ((*line)[0] == '\0')
+				ft_strdel(&(*line));
+		}
 		return (1);
 	}
-//	else
-//		ft_bzero((*line), 1);
+	else
+		ft_bzero((*line), 1);
 	return (fd - 1 || line == NULL ? -1 : 0);
 }
+
+// LE FREE LINE FAIT PASSER LES LEAKS DE 6 A 2. (En fait non, meme sans le free line les leaks sont a 2 bizzarement.)
+// GROS PROBLEME DE MALLOC SI LE TEXT EST TROP LONG. IL FAUT FIXE CA.
+//dans cette version, ca ne marche donc pas si le text est trop long, et ca leaks 2 fois.
+
