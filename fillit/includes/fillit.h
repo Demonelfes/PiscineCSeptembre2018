1f99cfa481da01aff2eac35b3a113de2529ed23c
Alopez <alexis.lopez.0605@gmail.com>
2019-05-07 10:23:32 +0200

    add first check

--- fillit/includes/fillit.h
+++ fillit/includes/fillit.h
@@ -1,3 +1,13 @@
-#include "libft/includes/libft.h"
+#ifndef FILLIT_H
+# define FILLIT_H
+# include "libft/includes/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
 
+typedef struct	s_tetri
+{
+	char	piece[4][2];
+	char	letter;
+}				t_tetri;
 
+int		check_hash(char *map);
--- fillit/srcs/main.c
+++ fillit/srcs/main.c
@@ -1,3 +1,5 @@
+#include "../includes/fillit.h"
+
 int		main()
 {
 	return (0);
